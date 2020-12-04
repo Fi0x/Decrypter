d7a3110897d8be79e3cc179f32fb6d6426a42a45
Fi0x <felpech@aol.com>
2020-12-04 11:39:57 +0100

    Added GUI elements
    Input field
    Start button
    Cancel button

--- src/main/java/com/fi0x/decrypter/Main.java
+++ src/main/java/com/fi0x/decrypter/Main.java
@@ -2,6 +2,8 @@ package com.fi0x.decrypter;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -14,6 +16,15 @@ public class Main extends Application
         primaryStage.setTitle("Decryption Program");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        TextField txtInput = new TextField();
+        root.add(txtInput, 0, 0);
+
+        Button btnStart = new Button("Start");
+        root.add(btnStart, 0, 1);
+
+        Button btnCancel = new Button("Cancel");
+        root.add(btnCancel, 1, 1);
     }
 
     public static void main(String[] args)
