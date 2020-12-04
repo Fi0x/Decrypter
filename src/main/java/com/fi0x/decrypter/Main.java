27c1f475b13e3ff3b6acad239e815bca5a058f2e
Fi0x <felpech@aol.com>
2020-12-04 12:16:39 +0100

    Re-added fxml
    added fxml file
    made gui nice
    added necessary functions to new Controller class

--- src/main/java/com/fi0x/decrypter/Main.java
+++ src/main/java/com/fi0x/decrypter/Main.java
@@ -1,30 +1,23 @@
 package com.fi0x.decrypter;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application
 {
     @Override
-    public void start(Stage primaryStage) throws Exception
+    public void start(Stage primaryStage) throws IOException
     {
-        GridPane root = new GridPane();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+
         primaryStage.setTitle("Decryption Program");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-        TextField txtInput = new TextField();
-        root.add(txtInput, 0, 0);
-
-        Button btnStart = new Button("Start");
-        root.add(btnStart, 0, 1);
-
-        Button btnCancel = new Button("Cancel");
-        root.add(btnCancel, 1, 1);
     }
 
     public static void main(String[] args)
