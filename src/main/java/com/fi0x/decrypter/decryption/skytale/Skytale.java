94b3af20b2cceab97fc74ebc5b750ccabf9494c9
Fi0x <felpech@aol.com>
2020-12-04 21:42:29 +0100

    Added Skytale decryption
    Implemented Skytale decryption
    Added alphabets to file

--- src/main/java/com/fi0x/decrypter/decryption/skytale/Skytale.java
+++ src/main/java/com/fi0x/decrypter/decryption/skytale/Skytale.java
@@ -1,14 +1,37 @@
 package com.fi0x.decrypter.decryption.skytale;
 
 import com.fi0x.decrypter.decryption.DecryptionHandler;
+import com.fi0x.decrypter.util.enums.CIPHER;
 
 public class Skytale implements Runnable
 {
+    private char[] encrypted;
+
     @Override
     public void run()
     {
         DecryptionHandler handler = DecryptionHandler.getInstance();
-        //TODO: Implement
+        encrypted = handler.getEncryptedString().toCharArray();
+        for(int i = 1; i < encrypted.length; i++)
+        {
+            handler.addDecryptedVersion(CIPHER.SKYTALE, decrypt(i));
+        }
+
         handler.removeRunning();
+    }
+
+    private String decrypt(int rows)
+    {
+        String decrypted = "";
+
+        for(int row = 0; row < rows; row++)
+        {
+            for(int i = 0; i < encrypted.length; i++)
+            {
+                if(i % rows == row) decrypted += encrypted[i];
+            }
+        }
+
+        return decrypted;
     }
 }
