7b8de16c4b40d59ee17a55136c5a3d98839b9bf6
Fi0x <felpech@aol.com>
2020-12-04 10:39:18 +0100

    Set up more
    Added manifest file
    updated pom file
    Made Main method work

--- src/main/java/com/fi0x/decrypter/Controller.java
+++
@@ -1,5 +0,0 @@
-package com.fi0x.decrypter;
-
-public class Controller
-{
-}
--- src/main/java/com/fi0x/decrypter/Main.java
+++ src/main/java/com/fi0x/decrypter/Main.java
@@ -1,23 +1,23 @@
 package com.fi0x.decrypter;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Main extends Application
+{
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) throws Exception
+    {
+        GridPane root = new GridPane();
+        primaryStage.setTitle("Decryption Program");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
